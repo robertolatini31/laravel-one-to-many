03a02df917404d15d4bf1145419346925025ca1b
robertolatini31 <robertolatini31@gmail.com>
2022-06-23 15:02:18 +0200

    add category_id column

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class Post extends Model
 {
     protected $fillable = ['title', 'content', 'img', 'slug'];
+
+    public function category(): BelongsTo {
+        return $this->belongsTo(Category::class);
+    }
 }
