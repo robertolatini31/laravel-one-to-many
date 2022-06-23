0e5debe184e4a7701d060407c71fc8d6effadafa
robertolatini31 <robertolatini31@gmail.com>
2022-06-23 14:28:18 +0200

    fix title edit

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -3,7 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-
+use Illuminate\Validation\Rule;
 class PostRequest extends FormRequest
 {
     /**
@@ -24,10 +24,10 @@ class PostRequest extends FormRequest
     public function rules()
     {
         return [
-            'title' => 'required|unique:posts|max:150',
-            'content' => 'nullable',
-            'img' => 'nullable',
-            'slug' => 'nullable'
+            'title' => ['required', Rule::unique('posts')->ignore('posts')|'max:150'],
+            'content' => ['nullable'],
+            'img' => ['nullable'],
+            'slug' => ['nullable']
         ];
     }
 }
